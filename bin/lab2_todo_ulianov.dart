54664143585e77e8dc4e56af540112bff28e20bc
ifgamek <andkategik@gmail.com>
2026-09-14 15:38:29 +0300

    initial dart project

---
+++ bin/lab2_todo_ulianov.dart
@@ -0,0 +1,5 @@
+import 'package:lab2_todo_ulianov/lab2_todo_ulianov.dart' as lab2_todo_ulianov;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lab2_todo_ulianov.calculate()}!');
+}
---
+++ test/lab2_todo_ulianov_test.dart
@@ -0,0 +1,8 @@
+import 'package:lab2_todo_ulianov/lab2_todo_ulianov.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
